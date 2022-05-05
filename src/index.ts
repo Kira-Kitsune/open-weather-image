d76cd1f2d9bea9310788c4d2ece17529d55b043b
Kira Pearce <cooperpearce@hotmail.com>
2022-05-06 01:11:24 +0930

    removed unneeded package, seperate current/today details into another function to be drawn for forecast to be added, all ctx is drawn except images to be incoorpated

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import axios, { AxiosError } from 'axios';
-import { createCanvas } from 'canvas';
+import { createCanvas, CanvasRenderingContext2D, loadImage } from 'canvas';
+
+type size = '1' | '2' | '4';
+const icon = (iconCode: string): string => {
+    return `http://openweathermap.org/img/wn/${iconCode}@1x.png`;
+};
 
 export interface IOpenWeatherImage {
     key: string;
@@ -7,6 +12,11 @@ export interface IOpenWeatherImage {
     stateCode?: string;
     countryCode?: string;
 }
+
+type TimeLocalised = {
+    date: string;
+    time: string;
+};
 
 const getResponse = async (URL: string): Promise<any> => {
     return await axios
@@ -30,87 +40,191 @@ const uvIndexServeness = (uvIndex: number): string => {
     return uviServeness;
 };
 
-const windArrow = (deg: number): string => {
-    let windArrow: string;
+const dir = (deg: number): string => {
+    let dir: string;
 
-    if (deg >= 0 && deg < 22.5) windArrow = '↑';
-    else if (deg >= 22.5 && deg < 67.5) windArrow = '↗';
-    else if (deg >= 67.5 && deg < 112.5) windArrow = '→';
-    else if (deg >= 112.5 && deg < 157.5) windArrow = '↘';
-    else if (deg >= 157.5 && deg < 202.5) windArrow = '↓';
-    else if (deg >= 202.5 && deg < 247.5) windArrow = '↙';
-    else if (deg >= 247.5 && deg < 292.5) windArrow = '←';
-    else if (deg >= 292.5 && deg < 337.5) windArrow = `↖`;
-    else if (deg >= 337.5 && deg <= 360) windArrow = '↑';
-    else windArrow = 'Error';
+    if (deg >= 0 && deg < 22.5) dir = 'N';
+    else if (deg >= 22.5 && deg < 67.5) dir = 'NE';
+    else if (deg >= 67.5 && deg < 112.5) dir = 'E';
+    else if (deg >= 112.5 && deg < 157.5) dir = 'SE';
+    else if (deg >= 157.5 && deg < 202.5) dir = 'S';
+    else if (deg >= 202.5 && deg < 247.5) dir = 'SW';
+    else if (deg >= 247.5 && deg < 292.5) dir = 'W';
+    else if (deg >= 292.5 && deg < 337.5) dir = `NW`;
+    else if (deg >= 337.5 && deg <= 360) dir = 'N';
+    else dir = 'Error';
 
-    return windArrow;
+    return dir;
 };
 
 const font = (size: number, name: string = 'Arial'): string => {
     return `${size}px ${name}`;
 };
 
-export const createWeatherImageToday = async (args: IOpenWeatherImage) => {
+const timestampConverter = (
+    timestamp: number,
+    timeZone: string
+): TimeLocalised => {
+    const date = new Date(timestamp * 1000);
+
+    const timeOptions: Intl.DateTimeFormatOptions = {
+        timeStyle: 'short',
+        timeZone,
+    };
+    const dateOptions: Intl.DateTimeFormatOptions = {
+        weekday: 'short',
+        month: 'long',
+        day: 'numeric',
+        timeZone,
+    };
+
+    return {
+        date: new Intl.DateTimeFormat('en-AU', dateOptions)
+            .format(date)
+            .replace(',', ''),
+        time: new Intl.DateTimeFormat('en-AU', timeOptions).format(date),
+    };
+};
+
+const capitaliseFirstLetter = (string: string): string => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export const createWeatherImageToday = async (
+    args: IOpenWeatherImage
+): Promise<string> => {
     const { key, cityName, stateCode, countryCode } = args;
 
     let query: string = cityName;
-    if (stateCode) query.concat(', ', stateCode);
-    if (countryCode) query.concat(', ', countryCode);
+    if (stateCode) query.concat(',', stateCode);
+    if (countryCode) query.concat(',', countryCode);
 
     const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${key}&units=metric&lang={en}`;
 
     const weatherResponse = await getResponse(WEATHER_URL);
 
-    const { coord, weather, main, wind, name, sys, timezone, cod } =
-        await weatherResponse;
+    const { coord } = await weatherResponse;
     const { lat, lon } = coord;
 
-    const FORECAST_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&appid=${key}&units=metric&lang={en}`;
+    const FORECAST_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${key}&units=metric&lang={en}`;
     const forecastResponse = await getResponse(FORECAST_URL);
 
-    const { daily } = forecastResponse;
-    const today = daily[0];
-    // const tomorrow = daily[1];
-    // const secondDay = daily[2];
-    // const thirdDay = daily[3];
-    // const fourthDay = daily[4];
+    const currentWidth = 520;
+    const canvasWidth = currentWidth;
+    const currentHeight = 300;
+    const canvasHeight = currentHeight;
 
-    const canvas = createCanvas(520, 300);
+    const canvas = createCanvas(canvasWidth, canvasHeight);
     const ctx = canvas.getContext('2d');
+
+    await createCurrentCtx(
+        ctx,
+        currentWidth,
+        currentHeight,
+        await weatherResponse,
+        await forecastResponse
+    );
+
+    return canvas.toDataURL();
+};
+
+const createCurrentCtx = async (
+    ctx: CanvasRenderingContext2D,
+    currentWidth: number,
+    currentHeight: number,
+    weatherResponse: any,
+    forecastResponse: any
+) => {
+    const { name, sys } = weatherResponse;
+    const { country } = sys;
+
+    const { current, daily, timezone } = forecastResponse;
+
+    const {
+        temp,
+        feels_like,
+        dt,
+        wind_speed,
+        wind_deg,
+        sunrise: sunriseDT,
+        sunset: sunsetDT,
+        humidity,
+        weather: currentWeather,
+    } = current;
+    const { description, icon: iconCurrent } = currentWeather[0];
+
+    const today = daily[0];
+    const { temp: tempToday, uvi, pop, weather: weatherToday } = today;
+    const { min: tempMin, max: tempMax } = tempToday;
+    const { icon: iconToday } = weatherToday[0];
 
     let leftPos: number;
 
     ctx.fillStyle = '#019AF3';
-    ctx.fillRect(0, 0, (canvas.width * 2) / 3 + 1, canvas.height);
+    ctx.fillRect(0, 0, (currentWidth * 2) / 3 + 1, currentHeight);
     ctx.fillStyle = '#0184D0';
-    ctx.fillRect((canvas.width * 2) / 3, 0, canvas.width, canvas.height);
+    ctx.fillRect((currentWidth * 2) / 3, 0, currentWidth, currentHeight);
 
-    leftPos = 22;
-    ctx.font = font(32);
+    // const imgToday = await loadImage(icon(iconToday));
+    // ctx.drawImage(imgToday, (currentWidth * 2) / 3, currentHeight / 2);
+
     ctx.fillStyle = 'black';
     ctx.strokeStyle = 'black';
-    ctx.fillText(`${name}, ${sys.country}`, leftPos, 52);
+
+    leftPos = 22;
+
+    ctx.font = font(32);
+    ctx.fillText(`${name}, ${country}`, leftPos, 62);
 
     ctx.font = font(16);
-    ctx.fillText(`Wed 22 August // 10:00pm`, leftPos, 85);
+    const { date, time } = timestampConverter(dt, timezone);
+    ctx.fillText(`${date} // ${time}`, leftPos, 88);
 
-    ctx.lineWidth = 3;
+    ctx.lineWidth = 0.5;
     ctx.beginPath();
     ctx.lineTo(15, 100);
     ctx.lineTo(304, 100);
     ctx.stroke();
 
     ctx.font = font(45);
-    ctx.fillText(`${main.temp}°C`, leftPos, 130);
+    const currentTemp: string = `${Math.round(temp)}°C`;
+    const { width: tempWidth } = ctx.measureText(currentTemp);
+    ctx.fillText(currentTemp, leftPos, 150);
+
+    ctx.font = font(16);
+    ctx.fillText(
+        `Feels Like: ${Math.round(feels_like)}°C`,
+        leftPos + tempWidth + 6,
+        150
+    );
 
     ctx.font = font(12);
-    ctx.fillText(`${today.temp.min} / ${today.temp.max}°C`, leftPos, 160);
+    ctx.fillText(
+        `${Math.round(tempMin)} / ${Math.round(tempMax)}°C`,
+        leftPos,
+        170
+    );
+
+    ctx.font = font(16);
+    ctx.fillText(capitaliseFirstLetter(description), 45, 191);
+
+    // const imgCurrent = await loadImage(icon(iconCurrent));
+    // ctx.drawImage(imgCurrent, leftPos, 191);
 
     ctx.beginPath();
     ctx.lineTo(15, 200);
     ctx.lineTo(304, 200);
     ctx.stroke();
 
-    console.log(canvas.toDataURL());
+    ctx.font = font(12);
+
+    const { time: sunrise } = timestampConverter(sunriseDT, timezone);
+    const { time: sunset } = timestampConverter(sunsetDT, timezone);
+    const tTab = `\u0009\u0009\u0009`;
+
+    ctx.fillText(`Wind: ${wind_speed}km/h (${dir(wind_deg)})`, leftPos, 218);
+    ctx.fillText(`Humidity: ${humidity}%`, leftPos, 233);
+    ctx.fillText(`UV Index: ${uvi} (${uvIndexServeness(uvi)})`, leftPos, 248);
+    ctx.fillText(`Chance of Rain: ${pop * 100}%`, leftPos, 263);
+    ctx.fillText(`Sunrise: ${sunrise}${tTab}Sunset: ${sunset}`, leftPos, 278);
 };
