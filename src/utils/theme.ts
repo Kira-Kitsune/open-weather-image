383e6233c0f06e897623e5feb776482f0e628d8d
Kira Kitsune <71082921+Kira-Kitsune@users.noreply.github.com>
2022-12-11 19:11:29 +0100

    Fixed Theme where no args passed

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -14,27 +14,41 @@ export class Theme {
     forecastBoxDivider: string;
 
     constructor(args?: ThemeInput) {
-        const {
-            dayThemeLeft,
-            dayThemeRight,
-            dayThemeText,
-            nightThemeLeft,
-            nightThemeRight,
-            nightThemeText,
-            forecastBgTheme,
-            forecastBoxTheme,
-            forecastText,
-            forecastBoxDivider,
-        } = args!;
-        this.dayThemeLeft = dayThemeLeft || '#FFD982';
-        this.dayThemeRight = dayThemeRight || '#5ECEF6';
-        this.dayThemeText = dayThemeText || 'black';
-        this.nightThemeLeft = nightThemeLeft || '#25395C';
-        this.nightThemeRight = nightThemeRight || '#1C2A4F';
-        this.nightThemeText = nightThemeText || 'white';
-        this.forecastBgTheme = forecastBgTheme || '#DDDDDD';
-        this.forecastBoxTheme = forecastBoxTheme || '#EEEEEE';
-        this.forecastText = forecastText || 'black';
-        this.forecastBoxDivider = forecastBoxDivider || 'black';
+        if (args) {
+            const {
+                dayThemeLeft,
+                dayThemeRight,
+                dayThemeText,
+                nightThemeLeft,
+                nightThemeRight,
+                nightThemeText,
+                forecastBgTheme,
+                forecastBoxTheme,
+                forecastText,
+                forecastBoxDivider,
+            } = args;
+
+            this.dayThemeLeft = dayThemeLeft || '#FFD982';
+            this.dayThemeRight = dayThemeRight || '#5ECEF6';
+            this.dayThemeText = dayThemeText || 'black';
+            this.nightThemeLeft = nightThemeLeft || '#25395C';
+            this.nightThemeRight = nightThemeRight || '#1C2A4F';
+            this.nightThemeText = nightThemeText || 'white';
+            this.forecastBgTheme = forecastBgTheme || '#DDDDDD';
+            this.forecastBoxTheme = forecastBoxTheme || '#EEEEEE';
+            this.forecastText = forecastText || 'black';
+            this.forecastBoxDivider = forecastBoxDivider || 'black';
+        }
+
+        this.dayThemeLeft = '#FFD982';
+        this.dayThemeRight = '#5ECEF6';
+        this.dayThemeText = 'black';
+        this.nightThemeLeft = '#25395C';
+        this.nightThemeRight = '#1C2A4F';
+        this.nightThemeText = 'white';
+        this.forecastBgTheme = '#DDDDDD';
+        this.forecastBoxTheme = '#EEEEEE';
+        this.forecastText = 'black';
+        this.forecastBoxDivider = 'black';
     }
 }
